2aeb52c376175829a87f6d3fa0fb990f1e7cf8ed
leanerWang <xinleical@163.com>
2023-05-27 22:11:40 +0800

    编译docs文件夹为网站根目录

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  //生产模式打包配置
+  build: {
+    outDir: 'docs', //Specify the output directory (relative to project root).
+  }
 })
