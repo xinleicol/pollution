adb30324f537233f29ed7a0545989b73a9013a66
leanerWang <xinleical@163.com>
2023-05-28 18:14:15 +0800

    主页面添加导航

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import SideBar from "./components/navs/SideBar.vue";
+import HeaderBar from "./components/navs/HeaderBar.vue";
 </script>
 
 <template>
-  <h1><a href="/gdwater_simulation/index.html">地下水污染模拟</a></h1>
-  
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="common-layout">
+    <el-container>
+      <el-header><HeaderBar></HeaderBar></el-header>
+      <el-container>
+        <el-aside width="200px">
+          <SideBar></SideBar>
+        </el-aside>
+
+        <el-main>
+          <h1><a href="/gdwater_simulation/index.html">地下水污染模拟</a></h1>
+          <div>
+            <a href="https://vitejs.dev" target="_blank">
+              <img src="/vite.svg" class="logo" alt="Vite logo" />
+            </a>
+            <a href="https://vuejs.org/" target="_blank">
+              <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+            </a>
+          </div>
+          <HelloWorld msg="Vite + Vue" />
+          <HelloWorld msg="Vite + Vue" />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
