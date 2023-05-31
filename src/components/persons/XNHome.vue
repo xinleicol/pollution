1e9d6756b66d492cb267c3efff9980f8c45a0d2d
leanerWang <xinleical@163.com>
2023-05-31 22:41:07 +0800

    集成vitepress静态博客

--- src/components/persons/XNHome.vue
+++ src/components/persons/XNHome.vue
@@ -16,7 +16,7 @@ import SideBar from "../navs/SideBar.vue";
           <img src="/vite.svg" class="logo" alt="Vite logo" />
         </a>
         <a href="https://vuejs.org/" target="_blank">
-          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+          <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
       </div>
       <HelloWorld msg="Vite + Vue" />
